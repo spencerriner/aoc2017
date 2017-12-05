a92880b87c557c7a82b104397eb4ca6286f5a7ac
Spencer Riner <spencer.c.riner@nasa.gov>
2017-12-05 09:02:49 -0600

    Day 2 first puzzle solved

--- day2a.py
+++ day2a.py
@@ -3,5 +3,12 @@
 
 import csv
 spreadsheet = csv.reader(open('day2input.txt', 'rb'), delimiter='\t')
+i = 0
+checksum = 0
 for row in spreadsheet:
-	print(row)
+    row = map(int, row)
+    highest = max(row)
+    lowest = min(row)
+    diff = int(highest) - int(lowest)
+    checksum += diff
+print "Final checksum: " + str(checksum)
